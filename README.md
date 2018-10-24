924923049251fb33abb9e0d79febb737ee01f0a7
Simon Wohlfahrt <simon.wohlfahrt@idalab.de>
2018-10-25 00:43:23 +0200

    few more checks, cleaning

--- README.md
+++ README.md
@@ -1,46 +1,51 @@
 
-# AWS helpers
+# AWS file share helper
 
-One Paragraph of project description goes here
+This script should help you creating an environment to share files with your client via Amazon S3.
 
 ### Prerequisites
 
 What things you need:
 
+- AWS User account with sufficient permissions. Whether being added to IAM group `S3ShareGroup` or you have permissions `S3FullAccess` and `IAMFullAccess` anyway (Administrator).
 
-- AWS account with permissions `S3FullAccess` and `IAMFullAccess`.
-> This is important since you want to create a new S3 bucket and an IAM user that has the appropriate permissions.
-- aws cli profile or aws credentials ready
-> Best practice would be to configure your aws profile with the aws-cli (command line interface). Read [this](https://docs.aws.amazon.com/cli/latest/userguide/cli-multiple-profiles.html) for more information.
-> `aws configure --profile "your-profile-name"`
+- aws cli profile or your aws credentials ready.
+
+> Best practice would be to configure your aws profile with the aws-cli (command line interface), so you won't have to enter your credentials as an input later.
+> `aws configure --profile "your-profile-name"`  
+Read [this](https://docs.aws.amazon.com/cli/latest/userguide/cli-multiple-profiles.html) for more information.
 
 
 ### Installing
 
-Get your Python env ready
+Just get your Python env ready .  
+`$ pipenv install ` 
 
-```
-$ pipenv install
-``` 
+`$ pipenv shell` 
 
-### Creating S3 bucket link + user with read/write only
-
-Execute the script `aws_S3_client_script.py`
-
+Then execute the script .  
 ```
 python aws_S3_client_script.py
 ```
-Follow the instructions and enter the required information:
-```
-1) Region for your S3 bucket (eu-central-1 is default)
-2) Aws profile (optional) or Aws credentials
-3) Bucket name
-4) User name
-5) User password
-Continue?: Enter "y" for yes
-Done.
-```
-The script will also create a `user-name.txt` file to store the login information that you have to send to the client.
+
+### Creating S3 bucket link + user with read/write only
+
+### How the script works
+At first the script will request some input information from the User and checks the permissions. 
+1) Region for your S3 bucket (eu-central-1 is default) .  
+2) Aws profile (optional) or Aws credentials. If you skip the profile option you have to enter your credentials (`Access Key ID`, `Secret Access Key`).   
+3) Bucket name: Here enter the name of the new bucket you want to use for data sharing with a client. In case you already created a bucket that you want to use, type its name and pass the warning with "y".   
+4) User name: This is the IAM user account for your client, e.g. "DB-demo-client".   
+5) User password: Set the password according to the printed policy, if there exists one.  
+6) Continue?: Last confirmation by the User before the AWS resources are actually created:  
+-   S3 bucket (unless you specify existing one)   
+- IAM User (initially without any permissions)  
+- User profile (user password)  
+- User policy (with allowed actions to put, get and delete objects)   
+
+Done! Bucket link and user login information will be printed and also stored in a file `<user-name>.txt`, ready to be sent out to the client.
+
+
 
 ## Authors
 
--- aws_S3_client_script.py
+++ aws_S3_client_script.py
@@ -11,6 +11,7 @@ import re
 from botocore.exceptions import ClientError
 from botocore.exceptions import ProfileNotFound
 
+ACCOUNT_ALIAS = 'idalab'
 VALID_REGIONS = ['ap-south-1', 'eu-west-3', 'eu-west-2', 'eu-west-1', 'ap-northeast-2', 'ap-northeast-1', 'sa-east-1',
                  'ca-central-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1', 'us-east-1', 'us-east-2',
                  'us-west-1', 'us-west-2']
@@ -22,19 +23,96 @@ BUCKET_NAME_PATTERN = '(?=^.{3,63}$)(?!^(\d+\.)+\d+$)' \
 
 
 def main():
-    print('INFO: Your AWS account should have permissions IAMFullAccess and AmazonS3FullAccess.\n'
-          '--> Starting AWS S3 client script...')
-    bucket_region = get_valid_region('Region (or press enter for {}): '.format(DEFAULT_REGION))
+    print('INFO: Your AWS User account should be added to IAM group "S3ShareGroup" or'
+          ' have permissions IAMFullAccess and AmazonS3FullAccess.\n'
+          '--> Starting AWS S3 client script... (you can quit with "\q")')
 
+    bucket_region = get_valid_region('Region (or press enter for {}): '.format(DEFAULT_REGION))
+    if bucket_region is None:
+        return
+    s3_client, s3_resource, iam_client = aws_login('Aws profile name (or enter to skip): ', bucket_region)
+    if s3_client is None:
+        return
+
+    all_bucket_names = get_all_bucket_names(s3_resource)
+    all_user_names = get_all_user_names(iam_client)
+    if all_bucket_names is None or all_user_names is None:
+        return
+
+    bucket_name, bucket_already_exists = get_valid_bucket_name('Name of S3 bucket: ', all_bucket_names)
+    user_name = get_valid_user_name('New User name (for bucket access): ', all_user_names)
+
+    print_password_policy(iam_client)
+    user_password = input('Set user password: ')
+
+    print('INFO: S3 bucket "{}" will be created in region "{}"'.format(bucket_name, bucket_region))
+    print('INFO: Iam user: "{}", password: "{}"'.format(user_name, user_password))
+
+    if continuation_prompt('Continue?', 'y'):
+        create_s3_bucket(s3_client, bucket_name, bucket_region, bucket_already_exists)
+        bucket_link = 'https://s3.console.aws.amazon.com/s3/buckets/{}/'.format(bucket_name)
+
+        # IAM USER STUFF
+        response = iam_client.create_user(UserName=user_name)
+        response = iam_client.create_login_profile(UserName=user_name, Password=user_password,
+                                                   PasswordResetRequired=CLIENT_PASSWORD_RESET_REQUIRED)
+        policy_json = create_bucket_user_policy(bucket_name)
+        response = iam_client.put_user_policy(PolicyDocument=policy_json, PolicyName=CLIENT_POLICY_NAME,
+                                              UserName=user_name)
+
+        login_filename = '{}_login.txt'.format(user_name)
+        response_string = 'Link to bucket: {}\nAccount alias: {}\nIAM User name: {}\nPassword: {}'.format(
+            bucket_link, ACCOUNT_ALIAS, user_name, user_password)
+
+        print('--> Successfully created S3 bucket and IAM User. Created login file "{}"'.format(login_filename))
+        print(response_string)
+        with open(login_filename, 'w') as f:
+            f.write(response_string)
+    else:
+        return
+
+
+def continuation_prompt(prompt, required_answer):
+    continuation_answer = input('{} ({}): '.format(prompt, required_answer))
+    if continuation_answer == required_answer:
+        return True
+    else:
+        print('--> Terminate process...')
+        return False
+        
+        
+def get_valid_region(prompt):
     while True:
-        aws_profile = input('Aws profile name (or enter to continue): ')
-        if aws_profile != '':
+        bucket_region = input(prompt)
+        if bucket_region == '\q':
+            print('--> Quit process...')
+            return None
+        elif bucket_region == '':
+            bucket_region = DEFAULT_REGION
+            print('--> Used default region "{}"'.format(DEFAULT_REGION))
+            break
+        elif bucket_region not in VALID_REGIONS:
+            print('--> ValueError: Region "{}" is not available. Please try again.\n'
+                  'INFO: Valid regions are: {}'.format(bucket_region, VALID_REGIONS))
+            continue
+        else:
+            print('--> Used region "{}"'.format(bucket_region))
+            break
+    return bucket_region
+
+
+def aws_login(prompt, bucket_region):
+    while True:
+        aws_profile = input(prompt)
+        if aws_profile == '\q':
+            print('--> Quit process...')
+            return None, None, None
+        elif aws_profile != '':
             try:
                 boto3.setup_default_session(profile_name=aws_profile, region_name=bucket_region)  # idalab, knorke
                 s3_client = boto3.client('s3')
                 s3_resource = boto3.resource('s3')
                 iam_client = boto3.client('iam')
-                ec2_client = boto3.client('ec2')
                 break
             except ProfileNotFound:
                 print('ProfileNotFoundError: profile "{}" does not exist. Please try again.\n'
@@ -49,62 +127,34 @@ def main():
             s3_client = boto3.client('s3', **config_dict)
             s3_resource = boto3.resource('s3',  **config_dict)
             iam_client = boto3.client('iam',  **config_dict)
-            ec2_client = boto3.client('ec2', **config_dict)
             break
+    return s3_client, s3_resource, iam_client
 
-    all_bucket_names = [bucket.name for bucket in s3_resource.buckets.all()]
-    all_user_names = [key['UserName'] for key in iam_client.list_users()['Users']]
 
-    bucket_name, bucket_already_exists = get_valid_bucket_name('Name of S3 bucket: ', all_bucket_names)
-    user_name = get_valid_user_name('User name: ', all_user_names)
-
-    print_password_policy(iam_client)
-    user_password = input('Set user password: ')
-
-    print('INFO: S3 bucket "{}" will be created in region "{}"'.format(bucket_name, bucket_region))
-    print('INFO: Iam user: "{}", password: "{}"'.format(user_name, user_password))
-    continue_answer = input('Continue? (y/n): ')
-
-    if continue_answer != 'y':
-        print('--> Terminate process...')
-        return
-    else:
-        create_s3_bucket(s3_client, bucket_name, bucket_region, bucket_already_exists)
-        bucket_link = 'https://s3.console.aws.amazon.com/s3/buckets/{}/'.format(bucket_name)
-
-        # IAM USER STUFF
-        response = iam_client.create_user(UserName=user_name)
-        response = iam_client.create_login_profile(UserName=user_name, Password=user_password,
-                                                   PasswordResetRequired=CLIENT_PASSWORD_RESET_REQUIRED)
-
-        policy_json = create_bucket_user_policy(bucket_name)
-        
-        response = iam_client.put_user_policy(PolicyDocument=policy_json, PolicyName=CLIENT_POLICY_NAME,
-                                              UserName=user_name)
-
-        login_filename = '{}_login.txt'.format(user_name)
-        with open(login_filename, 'w') as f:
-            f.write('Link to bucket: {}\nIAM User name: {}\nPassword: {}'.format(bucket_link, user_name, user_password))
-
-        print('--> Successfully created S3 bucket and IAM User. Created login file "{}"'.format(login_filename))
-        print('Link to bucket: {}\nIAM User name: {}\nPassword: {}'.format(bucket_link, user_name, user_password))
-        
-        
-def get_valid_region(prompt):
-    while True:
-        bucket_region = input(prompt)
-        if bucket_region == '':
-            bucket_region = DEFAULT_REGION
-            print('--> Used default region "{}"'.format(DEFAULT_REGION))
-            break
-        elif bucket_region not in VALID_REGIONS:
-            print('--> ValueError: Region "{}" is not available. Please try again.\n'
-                  'INFO: Valid regions are: {}'.format(bucket_region, VALID_REGIONS))
-            continue
+def get_all_bucket_names(s3_resource):
+    try:
+        return [bucket.name for bucket in s3_resource.buckets.all()]
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDenied":
+            print('ClientError (AccessDenied): You are not allowed to list the S3 buckets.'
+                  ' Ask admin to add you username to the IAM group!')
         else:
-            print('--> Used region "{}"'.format(bucket_region))
-            break
-    return bucket_region
+            raise e
+        print('--> Terminate process...')
+        return None
+
+
+def get_all_user_names(iam_client):
+    try:
+        return [key['UserName'] for key in iam_client.list_users()['Users']]
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDenied":
+            print('ClientError (AccessDenied): You are not allowed to list users.'
+                  ' Ask admin to add you username to the IAM group!')
+        else:
+            raise e
+        print('--> Terminate process...')
+        return None
 
 
 def get_valid_bucket_name(prompt, all_bucket_names):
